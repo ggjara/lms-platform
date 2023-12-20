33277c80a7100eed7585e988acf89f59a8b73f5b
Gonzalo Jara <gonzalojarasaba@gmail.com>
2023-12-20 18:01:35 -0500

    Initial commit

---
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -0,0 +1,30 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
+import { auth } from "@clerk/nextjs";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+
+const CoursesPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/")
+  }
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    }
+  })
+  return (
+    <div className="p-6">
+      <DataTable columns={columns} data={courses} />
+    </div>
+  );
+}
+ 
+export default CoursesPage;
---
+++ scripts/seed.ts
@@ -0,0 +1,26 @@
+const { PrismaClient } = require('@prisma/client');
+
+const database = new PrismaClient();
+
+async function main() {
+  try {
+    await database.category.createMany({
+      data: [
+        { name: "Computer Science" },
+        { name: "Music" },
+        { name: "Fitness" },
+        { name: "Photography" },
+        { name: "Accounting" },
+        { name: "Engineering" },
+        { name: "Filming" },
+      ]
+    })
+    console.log("Sucess seeding the database categories")
+  } catch (error) {
+    console.log("Error seedinf the database catgoeries", error)
+  } finally {
+    await database.$disconnect();
+  }
+}
+
+main();
